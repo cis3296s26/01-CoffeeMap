27a521855a7898a5544ea468ac86581e8946c32f
rachelbuttimer <rachel.buttimer@gmail.com>
2026-03-16 18:27:15 -0400

    updated sign up and added log in page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+import {useState, useEffect} from 'react';
+import {onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+
 export default function App() {
   return (
       <div>
