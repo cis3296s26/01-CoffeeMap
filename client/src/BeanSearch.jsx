4ca139777ea11d70a6da734ce6923429773c4085
Ellie Han <ellie.han@temple.edu>
2026-03-17 23:33:45 -0400

    Update BeanSearch.jsx

--- client/src/BeanSearch.jsx
+++ client/src/BeanSearch.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
-export default function Search() {
-    
+export default function BeanSearch() {
+    // initialize variables
+
+    // extract data 
+
+    // compile database using extracted data
+
+    // search through database using search bar input
+
+    // frontend design
 }
