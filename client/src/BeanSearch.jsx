3b1c509c1b82398e637640e60d0f3d17b96bfb5f
Ellie Han <ellie.han@temple.edu>
2026-03-17 23:55:52 -0400

    Update BeanSearch.jsx

--- client/src/BeanSearch.jsx
+++ client/src/BeanSearch.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
+import Papa from 'papaparse'
 
 export default function BeanSearch() {
     // initialize variables
+    const [beans, setBeans] = useState([])
+    const [query, setQuery] = useState("")
 
     // extract data 
+    useEffect(() => {
+        const arabica_csv = "https://github.com/jldbc/coffee-quality-database/blob/master/data/arabica_data_cleaned.csv"
+        // add robusta later
 
-    // compile database using extracted data
+        Papa.parse(arabica_csv, {
+            download: true,
+            header: true,
+            complete: function(results) {
+                // compile database using extracted data
+                const database = results.data
+                setBeans(database)
+            }
+        })
+
+    }, [])
 
     // search through database using search bar input
 
