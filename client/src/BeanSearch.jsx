ea9a5ff64ff84f40dccf2128b06630c231f37f10
Ellie Han <ellie.han@temple.edu>
2026-03-21 11:12:04 -0400

    Update BeanSearch.jsx

--- client/src/BeanSearch.jsx
+++ client/src/BeanSearch.jsx
@@ -10,9 +10,18 @@ export default function BeanSearch() {
     // extract data 
     useEffect(() => {
         const arabica_csv = "https://raw.githubusercontent.com/jldbc/coffee-quality-database/master/data/arabica_data_cleaned.csv"
-        // add robusta later
+        const robusta_csv = "https://raw.githubusercontent.com/jldbc/coffee-quality-database/refs/heads/master/data/robusta_data_cleaned.csv"
 
                 Papa.parse(arabica_csv, {
+                    download: true,
+                    header: true,
+                    complete: function(results) {
+                        // compile database using extracted data
+                        const database = results.data
+                        setBeans(database)
+                    }
+                })
+                Papa.parse(robusta_csv, {
                     download: true,
                     header: true,
                     complete: function(results) {
