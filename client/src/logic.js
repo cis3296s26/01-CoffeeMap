7fc7e66a4964ede384ba6ef638f0f7ca47d277d0
Luana Cardoso da Silva <luuuuanacarolina@gmail.com>
2026-03-25 08:32:45 -0400

    Bug fix

--- client/src/logic.js
+++ client/src/logic.js
@@ -1,23 +1,22 @@
 //Filter function
-const applyFilters = (bean, filters) => {
-    const matchCounty = !filters.country || bean["Country.of.Origin"] === filters.country
-    const matchRegion = !filters.region || bean["Region"] === filters.region
-    const matchAroma = !filters.aroma || bean["Aroma"] === filters.aroma
+export const applyFilters = (bean, filters) => {
+    const matchCounty = !filters.country || bean["Country.of.Origin"] === filters.country;
+    const matchRegion = !filters.region || bean["Region"] === filters.region;
+    const matchAroma = !filters.aroma || bean["Aroma"] === filters.aroma;
 
-    return matchCounty && matchRegion && matchAroma
-}
+    return matchCounty && matchRegion && matchAroma;
+};
 
 //search through database using search bar input
-const searchBeans = (beans, query, filters) => {
-    const lowerQuery = query.toLowerCase()
+export const searchBeans = (beans, query, filters) => {
+    const lowerQuery = query.toLowerCase();
 
     return beans.filter((bean) => {
         const matchSearch = 
             bean["Country.of.Origin"]?.toLowerCase().includes(lowerQuery) ||
             bean["Region"]?.toLowerCase().includes(lowerQuery) ||
-            bean["Variety"]?.toLowerCase().includes(lowerQuery)
-        return matchSearch && applyFilters(bean, filters)
-    })
-}
-
-module.exports = { applyFilters, searchBeans }
+            bean["Variety"]?.toLowerCase().includes(lowerQuery);
+            
+        return matchSearch && applyFilters(bean, filters);
+    });
+};
--- client/src/logic.test.js
+++ client/src/logic.test.js
@@ -1,30 +1,27 @@
-const { applyFilters, searchBeans } = require("./logic");
+import { applyFilters, searchBeans } from "./logic";
 
 const beansSample = [
     { "Country.of.Origin": "Ethiopia", "Region": "Sidamo", "Variety": "guji-hambela", "Aroma": "8.25" },
     { "Country.of.Origin": "Brazil", "Region": "south of minas", "Variety": "Bourbon", "Aroma": "8.0" }
-]
+];
 
-//Test applyFilters function
 describe("applyFilters", () => {
     test('Returns true if country matches filter', () => {
-        const filters = { country: "Ethiopia", region: "", aroma: "" }
-        expect(applyFilters(beansSample[0], filters)).toBe(true)
-    })
-})
+        const filters = { country: "Ethiopia", region: "", aroma: "" };
+        expect(applyFilters(beansSample[0], filters)).toBe(true);
+    });
+});
 
-//Test searchBeans function
 describe('searchBeans', () => {
     test('returns all beans when query is empty', () => {
-        const filters = { country: "", region: "", aroma: "" }
-        const result = searchBeans(beansSample, "", filters)
-        expect(result.length).toBe(2) //2 beans in the beansSample
-    })
+        const filters = { country: "", region: "", aroma: "" };
+        const result = searchBeans(beansSample, "", filters);
+        expect(result.length).toBe(2); //2 beans in sample data
+    });
 
     test('Searching "brazil" finds "Brazil"', () => {
-        const filters = { country: "", region: "", aroma: "" }
-        const result = searchBeans(beansSample, "brazil", filters)
-        expect(result[0]["Country.of.Origin"]).toBe("Brazil")
-    })
-
-})
+        const filters = { country: "", region: "", aroma: "" };
+        const result = searchBeans(beansSample, "brazil", filters);
+        expect(result[0]["Country.of.Origin"]).toBe("Brazil");
+    });
+});
