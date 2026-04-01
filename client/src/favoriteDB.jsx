4a57454a3c9d830cfa89b3ef50a6275fd55ba2d1
Luana Cardoso da Silva <luuuuanacarolina@gmail.com>
2026-03-31 21:08:59 -0400

    favorite add, remove, get

---
+++ client/src/favoriteDB.jsx
@@ -0,0 +1,32 @@
+import {db} from "./firebase"; 
+import {doc, setDoc, deleteDoc, collection, onSnapshot} from "firebase/firestore";
+
+
+//Save favorite options
+export async function saveFavorite(userId, bean){
+    const beanData = {
+        country: bean["Country.of.Origin"],
+        region: bean["Region"],
+        variety: bean["Variety"],
+        aroma: bean["Aroma"],
+        score: bean["Total.Cup.Points"]
+    }
+    const beanId = `${bean["Country.of.Origin"]}_${bean["Region"]}`.replace(/\s+/g, '_');
+    await setDoc(doc(db, "users", userId, "favorites", beanId), beanData);
+}
+
+//Remove favorite options
+export async function removeFromFavorites(userId, bean){
+    const favoriteBean = `${bean["country"]}_${bean["region"]}`.replace(/\s+/g, '_');
+    await deleteDoc(doc(db, "users", userId, "favorites", favoriteBean));
+}
+
+//Get favorite options from spcecific user
+export function getFavorites(userId, callback){
+    const favoriteBeansRef = collection(db, "users", userId, "favorites");
+    return onSnapshot(favoriteBeansRef, (snapshot) => {
+        const favorites = snapshot.docs.map(doc => doc.data())
+        callback(favorites);
+    })
+
+}
