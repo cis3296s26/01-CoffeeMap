af41299582c1d69e8c28f08949593c19ff813f08
rachelbuttimer <rachel.buttimer@gmail.com>
2026-04-01 10:06:10 -0400

    fixed main

--- client/src/NavigationBar.jsx
+++ client/src/NavigationBar.jsx
@@ -26,6 +26,7 @@ export default function NavigationBar() {
                     <li><Link to="/" style={{color: "white", textDecoration: "none"}}>Home</Link></li>
                     <li><Link to="/about" style={{color: "white", textDecoration: "none"}}>About</Link></li>
                     <li><Link to="/search" style={{color: "white", textDecoration: "none"}}>Search</Link></li>
+                    <li><Link to="/favorites" style={{color: "white", textDecoration: "none"}}>Favorites</Link></li>
                     <li><Link to="/signup" style={{color: "white", textDecoration: "none"}}>Sign Up</Link></li>
                     <li><Link to="/login" style={{color: "white", textDecoration: "none"}}>Log In</Link></li>
                     {isAuthenticated && userData && (
